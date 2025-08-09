cedc5044fbf3f220c5b9c97b090477b34b1f336d
Justin Wang <justin20061023@gmail.com>
2025-08-09 18:34:51 -0500

    backend

---
+++ bot-backend.py
@@ -0,0 +1,25 @@
+import discord
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+class MyClient(discord.Client):
+  async def on_ready(self):
+    print(f'Logged on as {self.user}!')
+
+  async def on_message(self, message):
+    if message.author == self.user:
+      return
+    if message.content.startswith('$meme'):
+      await message.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('MTQwMzg2NDI0NjU2MzExNTEzMg.GZVyQQ.a-POEL7nBAzIT3D_7ZfQ7vT0g2mCdqjHefOL7A')
+
